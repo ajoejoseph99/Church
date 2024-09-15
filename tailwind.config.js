73c1196a7840a76658f7d3e687be91bc43602e20
Ajoe Joseph <ajoejoseph99@gmail.com>
2024-09-15 11:17:55 +0530

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shRed: '#770c0d',
+      },
+    },
   },
   plugins: [],
 }
